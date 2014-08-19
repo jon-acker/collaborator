3dd7bac6f427de90c2a1804150cf1d3693969dba
Jon Acker <jacker@inviqa.com>
2014-08-19 09:59:28 +0100

    Register spec task in gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+    'use strict';
+    // Project configuration.
+    grunt.initConfig({
+        jasmine : {
+            src : ['src/**/*.js'],
+            options : {
+                keepRunner: true,
+                specs : 'spec/**/*.js',
+                template: require('grunt-template-jasmine-requirejs'),
+                templateOptions: {
+                    requireConfigFile: 'collaborator/collaborator-requirejs-config.js',
+                    requireConfig: { }
+                }
+            }
+        }
+    });
+
+	grunt.loadTasks('tasks');
+
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-spec');
+
+	grunt.registerTask('default', ['jasmine']);
+
+};
