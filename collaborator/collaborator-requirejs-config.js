2a1b3318e0b08e2e4f37705013915df139e4b0c6
Jon Acker <jacker@inviqa.com>
2014-09-01 11:33:27 +0100

    Missing config

--- collaborator/collaborator-requirejs-config.js
+++ collaborator/collaborator-requirejs-config.js
@@ -1,1 +1,14 @@
+var grunt = require('grunt');
+require.config({
+
+    deps: [
+        'collaborators',
+        'collaborator/builder'
+    ],
+    callback: function(collaborators, collaboratorBuilder) {
+		require.config({
+            map: collaboratorBuilder.createDependencyMap(collaborators)
+        });
+    }
+});
 
