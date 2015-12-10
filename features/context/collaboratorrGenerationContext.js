5b0bf7e231d593c5c4ffee7a623c5023a4e11c5d
Jon Acker <jacker@inviqa.com>
2015-12-10 14:03:14 +0000

    Fix failing scenario

--- features/context/collaboratorrGenerationContext.js
+++ features/context/collaboratorrGenerationContext.js
@@ -5,9 +5,4 @@ module.exports = function() {
     this.Given(/^there are no collaborators/, function (callback) {
         processSpawner.exec('rm -rf collaborators.yml requirements.yml', callback);
     });
-
-    this.When(/^I say that I do want to set up the collaborator$/, function (callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback.pending();
-    });
-}
+};
